40e12a2842fe65709ceddb997343534e7189d9b6
Prabin Rath <prabinrath123@gmail.com>
2022-05-20 12:05:11 +0530

    odometry perfected

--- scripts/armbot_nav_teleop.py
+++ scripts/armbot_nav_teleop.py
@@ -193,8 +193,8 @@ if __name__=="__main__":
 
     rospy.init_node('armbot_nav_teleop')
 
-    speed = rospy.get_param("~speed", 0.5)
-    turn = rospy.get_param("~turn", 1.0)
+    speed = rospy.get_param("~speed", 0.2)
+    turn = rospy.get_param("~turn", 0.2)
     repeat = rospy.get_param("~repeat_rate", 0.0)
     key_timeout = rospy.get_param("~key_timeout", 0.0)
     stamped = rospy.get_param("~stamped", False)
